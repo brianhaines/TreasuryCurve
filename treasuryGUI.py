2f7c959e55d489086cba4c3465fd82679e32f9e3
DevOnLinux <b.ge.haines@gmail.com>
2014-07-28 18:07:51 -0400

    Added button and label code

--- treasuryGUI.py
+++ treasuryGUI.py
@@ -3,9 +3,9 @@ import tkinter
 import threading
 import time
 
-x = XML.curveGet()
-print(x.id[1])
-print(x.month3[1])
+#x = XML.curveGet()
+#print(x.id[1])
+#print(x.month3[1])
 
 class getCurveValues(threading.Thread):
 	def __init__(self):
@@ -15,11 +15,12 @@ class getCurveValues(threading.Thread):
 	def run(self):
 		#Things here:
 
-		while not self._abort:
-			next(self._nPhotos)
+		#while not self._abort:
+
+			
 
 	def abort(self):
-	self._abort = True
+		self._abort = True
 
 
 class showTreasuryCurve(tkinter.Tk):
@@ -33,19 +34,28 @@ class showTreasuryCurve(tkinter.Tk):
 	def initialize(self):
 		self.grid()
 
+		self.source_string = tkinter.StringVar()
+
+		#Define the text box where the source folder path is displayed
+		self.source = tkinter.Label(self, textvariable="1month or more if possible", anchor='w', fg='white', bg='blue')
+		self.source.grid(row = 0, column = 0, sticky = 'W')
+		self.source_string.set("1month")
+
+		#Source button
+		source_button = tkinter.Button(self, text="Retrieve Curve", command=self.getCurve())
+		source_button.grid(row = 4, column = 1, sticky = 'W')
+
 	def getCurve(self):
-		self.worker = function(object)
+		self.worker = getCurveValues()
 		self.worker.start()
 		while self.worker.isAlive(): # We wait for the worker to stop.
 			time.sleep(1)
 
 
 
-
-
 if __name__=="__main__":
-	app = photosort_app(None) #Here parent is None because this is the 
+	app = showTreasuryCurve(None) #Here parent is None because this is the 
 							 #first GUI element created
-	app.title("PhotoSorter")
+	app.title("Treasury Curve")
 	app.mainloop()
 
